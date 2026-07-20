96dcf25c37c963a44a07df04f5a877e317c16957
kanak <kanakjain639@gmail.com>
2026-07-20 20:42:35 +0530

    day-1

---
+++ variable-function.js
@@ -0,0 +1,36 @@
+// var x = 7;
+
+// function getName() {
+//   console.log("namaste idiots");
+// }
+
+// getName();
+// console.log(x);
+
+// --------------------------
+// var city = "Agra";
+
+// function greetUser() {
+//   console.log("Welcome to the city!");
+// }
+
+// greetUser();
+// console.log(city);
+
+// ----------------------------
+// var age = 26;
+// function sayHello() {
+//     console.log("Hi there!");
+// }
+// sayHello();
+// console.log(age);
+// ----------------------------
+getName();
+console.log(x);
+
+var x = 7;
+
+function getName() {
+  console.log("namaste idiots");
+}
+
